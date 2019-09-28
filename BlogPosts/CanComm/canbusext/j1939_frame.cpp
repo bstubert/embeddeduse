301625af36d5bcc35ae4df7f6fb71d297585f5c3
Burkhard Stubert <burkhard.stubert@embeddeduse.com>
2019-09-28 13:54:44 +0200

    Replaced magic numbers for mask and shift by constants.

--- BlogPosts/CanComm/canbusext/j1939_frame.cpp
+++ BlogPosts/CanComm/canbusext/j1939_frame.cpp
@@ -2,6 +2,17 @@
 
 #include "j1939_frame.h"
 
+namespace
+{
+constexpr quint32 MASK_PRIORITY{0x1C000000U};
+constexpr quint32 SHIFT_PRIORITY{26U};
+constexpr quint32 MASK_PDU_FORMAT{0x01FF0000U};
+constexpr quint32 SHIFT_PDU_FORMAT{16U};
+constexpr quint32 MASK_PDU_SPECIFIC{0x0000FF00U};
+constexpr quint32 SHIFT_PDU_SPECIFIC{8U};
+constexpr quint32 MASK_SOURCE_ADDRESS{0x000000FFU};
+}
+
 J1939Frame::J1939Frame(quint8 priority, quint16 pduFormat, quint8 pduSpecific, quint8 sourceAddress)
     : QCanBusFrame{toJ1939FrameId(priority, pduFormat, pduSpecific, sourceAddress), {}}
 {
@@ -14,22 +25,22 @@ J1939Frame::J1939Frame(quint8 priority, quint16 pduFormat, quint8 pduSpecific, q
 
 quint8 J1939Frame::priority() const
 {
-    return static_cast<quint8>((frameId() & 0x1C000000U) >> 26);
+    return static_cast<quint8>((frameId() & MASK_PRIORITY) >> SHIFT_PRIORITY);
 }
 
 quint16 J1939Frame::pduFormat() const
 {
-    return static_cast<quint16>((frameId() & 0x01FF0000U) >> 16);
+    return static_cast<quint16>((frameId() & MASK_PDU_FORMAT) >> SHIFT_PDU_FORMAT);
 }
 
 quint8 J1939Frame::pduSpecific() const
 {
-    return static_cast<quint8>((frameId() & 0x0000FF00U) >> 8);
+    return static_cast<quint8>((frameId() & MASK_PDU_SPECIFIC) >> SHIFT_PDU_SPECIFIC);
 }
 
 quint8 J1939Frame::sourceAddress() const
 {
-    return static_cast<quint8>(frameId() & 0x000000FFU);
+    return static_cast<quint8>(frameId() & MASK_SOURCE_ADDRESS);
 }
 
 
@@ -37,8 +48,8 @@ quint32 J1939Frame::toJ1939FrameId(quint8 priority,
         quint16 pduFormat, quint8 pduSpecific, quint8 sourceAddress
         ) const
 {
-    return (quint32{priority} << 26) | (quint32{pduFormat} << 16) | (quint32{pduSpecific} << 8) |
-            (quint32{sourceAddress});
+    return (quint32{priority} << SHIFT_PRIORITY) | (quint32{pduFormat} << SHIFT_PDU_FORMAT) |
+            (quint32{pduSpecific} << SHIFT_PDU_SPECIFIC) | (quint32{sourceAddress});
 }
 
 bool J1939Frame::isJ1939Compliant(quint16 pduFormat) const
